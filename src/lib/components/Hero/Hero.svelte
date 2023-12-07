63c11dd4bc7c5771e8e3fc377a0076597ea3be40
Tom Nightingale <tom@weareleague.co.uk>
2023-12-07 14:09:11 +0000

    feat(Hero): Add Glide slider

--- src/lib/components/Hero/Hero.svelte
+++ src/lib/components/Hero/Hero.svelte
@@ -1,65 +1,75 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { browser } from "$app/environment";
   import Image from "$lib/components/Image/Image.svelte";
   import type { Image as ImageType } from "$lib/gql/gen/codegen";
   import Typography from "$lib/components/Typography/Typography.svelte";
   import Button from "$lib/components/Button/Button.svelte";
+  import Glide from "@glidejs/glide";
 
   export let images: ImageType[] | null | undefined;
   export let title: string | null | undefined;
   export let isHome: boolean = false;
 
-  onMount(() => {});
+  onMount(() => {
+    if (images && images.length > 0) {
+      new Glide(".glide", {
+        perView: 1,
+        gap: 10,
+        autoplay: false,
+        bound: true,
+        rewind: false,
+      }).mount();
+    }
+  });
 </script>
 
 <div class="relative w-full">
   <div class="relative overflow-hidden bg-gray-100">
     {#if images && images.length > 0}
-      <!-- <swiper-container
-        init="false"
-        bind:this={swiperEl}
-        autoplay-disable-on-interaction={false}
-      >
-        {#each images as image, i}
-          <swiper-slide>
-            <div class="relative overflow-y-hidden bg-gray-100 hero-container">
-              <Image
-                lazyLoad={false}
-                {image}
-                altText={image?.asset?.altText
-                  ? image?.asset?.altText
-                  : "Coleflax Bennett Architecture"}
-                lgImg={true}
-                lgSizes={{
-                  lg: {
-                    width: 2560,
-                    height: 1175,
-                  },
-                  md: {
-                    width: 1600,
-                    height: 760,
-                  },
-                  sm: {
-                    width: 1280,
-                    height: 1000,
-                  },
-                  xs: {
-                    width: 768,
-                    height: 1000,
-                  },
-                  fallback: {
-                    width: 768,
-                    height: 1622,
-                  },
-                }}
-                pictureClasses="block"
-                imageClasses="absolute top-0 left-0 w-full h-full object-cover object-center w-full"
-              />
-            </div>
-          </swiper-slide>
-        {/each}
-      </swiper-container> -->
+      <div class="relative glide">
+        <div data-glide-el="track" class="glide__track">
+          <div class="glide__slides">
+            {#each images as image, i}
+              <div
+                class="relative overflow-y-hidden bg-gray-100 hero-container"
+              >
+                <Image
+                  lazyLoad={false}
+                  {image}
+                  altText={image?.asset?.altText
+                    ? image?.asset?.altText
+                    : "Coleflax Bennett Architecture"}
+                  lgImg={true}
+                  lgSizes={{
+                    lg: {
+                      width: 2560,
+                      height: 1175,
+                    },
+                    md: {
+                      width: 1600,
+                      height: 760,
+                    },
+                    sm: {
+                      width: 1280,
+                      height: 1000,
+                    },
+                    xs: {
+                      width: 768,
+                      height: 1000,
+                    },
+                    fallback: {
+                      width: 768,
+                      height: 1622,
+                    },
+                  }}
+                  pictureClasses="block"
+                  imageClasses="absolute top-0 left-0 w-full h-full object-cover object-center w-full"
+                />
+              </div>
+            {/each}
+          </div>
+        </div>
+      </div>
     {:else if images && images[0] && images[0].asset}
       <div class="relative overflow-y-hidden bg-gray-100 hero-container">
         <Image
